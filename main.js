c7de252ebde08b10e2596bb4785a99fc89e5182c
Nirma Abro <107070067+NirmaAbro@users.noreply.github.com>
2023-09-22 23:22:13 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+ // Function to smoothly scroll to a section by its ID
+ function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+        window.scrollTo({
+            top: section.offsetTop,
+            behavior: 'smooth'
+        });
+    }
+}
